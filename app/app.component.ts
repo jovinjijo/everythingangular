ede2fcaeb05593df350331fcb74c31156eddf3eb
root <root@INLN34345089A.apj.global.corp.sap>
2020-06-01 12:25:14 +0000

    changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
     import { Component } from '@angular/core';
     @Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      template: '<h1>My First Angular 2 App</h1><h2>Hi</h2>'
     })
     export class AppComponent { }
